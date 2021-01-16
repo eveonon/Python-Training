aa25a75afddbd129472a55d60a7518610f613b40
Yvonne <eveonon@yahoo.com>
2021-01-16 16:46:22 -0600

    [Yvonne] - Commit Lab 3 task.

---
+++ lab1.py
@@ -0,0 +1,3 @@
+print ("I am a professional.")
+print ("I am victorious.")
+print ("I am a developer.")
---
+++ lab3.py
@@ -0,0 +1,17 @@
+a,b = 24,25
+print("( a = 24", ",", "b = 25 )" , "a", "+", "b", "=", a + b)
+c, d = 23 , 12
+print("( c = 23", ",", "d = 12 )", "c", "*", "d", "=", c * d )
+x = 10
+y = 0.5
+print("( x = 10", ",", "y = 0.5 )", "x", "*", "y", "=", x + y)
+Price = 40
+Price = Price + 5
+print(Price)
+sales = 5
+print (Price - sales)
+initial_price = 50
+sales_percent = 20/100
+sale_price = initial_price - (sales_percent*initial_price)
+print("The original price was " + str(initial_price) + ". With a sale of 20 percent,your current price is " + str(sale_price))
+print("$ " + str(sale_price))
