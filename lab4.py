9148ba7d75ce6622f1556103c29b5198a8f419fa
Yvonne <eveonon@yahoo.com>
2021-01-23 17:17:42 -0600

    [Yvonne] - Commiting lab 4

---
+++ lab4.py
@@ -0,0 +1,17 @@
+#Lists
+
+Songs = ['Breathe', 'Ebube', 'Kingdom Now', 'I proclaim', 'Ageless', 'Open Up', 'Lifter', 'Imole de', 'The Anthem', 'Forever']
+print(Songs)
+
+
+#Dictionary
+print("\n\nDictionary is used to map values and retrieve specific values")
+cloth_dict = {'Gray': 'Casual', 'white': 'Church', 'Blue': 'Work'}
+print(cloth_dict)
+print(cloth_dict['Blue'])
+
+#Set
+print("\n\nSet is used to retrieve unique values")
+
+set_of_values = set('appleorangeblack')
+print(set_of_values)
