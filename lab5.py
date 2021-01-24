4e0effcc1f2a34f06501d0b3195dcb0b98d9c45b
Yvonne <eveonon@yahoo.com>
2021-01-23 18:49:30 -0600

    [Yvonne] - Commiting lab 5

---
+++ lab5.py
@@ -0,0 +1,21 @@
+#2
+
+
+#3
+cloth_dict = {'Gray': 'Casual', 'white': 'Church', 'Blue': 'Work'}
+def dictionary(cloth_dict, num2):
+    return [num2]
+
+print (dictionary(cloth_dict, num2 ='white'))
+
+
+#4
+x = [4, 5, 6]
+y = [6, 7, 8]
+
+def set_of_values (x, y):
+    values = [x + y]
+    return (values)
+
+result = set_of_values (x,y)
+print(result)
